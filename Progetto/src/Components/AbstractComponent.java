e6705ae58530bd33df05de516a08ec066dd63366
acurtoni <YESYESYES28>
2019-04-09 15:42:05 +0200

    attributi di AbstractComponent

--- Progetto/src/Components/AbstractComponent.java
+++ Progetto/src/Components/AbstractComponent.java
@@ -1,4 +1,28 @@
 package Components;
 
-public abstract class AbstractComponent {
+public abstract class AbstractComponent implements Comparable {
+
+    protected String ID;
+    protected double price;
+    protected int performance;
+    protected String brand;
+    protected String name;
+    protected int quantity;
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractComponent{" +
+                "ID='" + ID + '\'' +
+                ", price=" + price +
+                ", performance=" + performance +
+                ", brand='" + brand + '\'' +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
