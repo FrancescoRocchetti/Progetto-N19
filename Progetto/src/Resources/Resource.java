92c296aa8adbe0ee09f57ce1669d8233dee4412b
Manuel Zippo <manuel.zippo01@universitadipavia.it>
2019-04-09 15:58:25 +0200

    /

--- Progetto/src/Resources/Resource.java
+++ Progetto/src/Resources/Resource.java
@@ -1,5 +1,6 @@
 package Resources;
 
+//LA CLASSE E' ANCORA INCOMPLETA
 public class Resource {
     private String typeSocket;
     private int RAMmodules;
---
+++ Progetto/src/TypeComponent.java
@@ -0,0 +1,3 @@
+public enum  TypeComponent {
+    CASE, COOLER, CPU, FAN, GPU, INPUTOUTPUT, MOBO, OS, PSU, RAM, STORAGE, SW
+}
