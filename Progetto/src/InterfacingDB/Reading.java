92d939e75735b21e2ae928b47a42ac22ae8018c5
cl448776 <fabio.riganti01@universitadipavia.it>
2019-04-11 15:41:06 +0200

    Aggiunto commento alla classe Reading

--- Progetto/src/InterfacingDB/Reading.java
+++ Progetto/src/InterfacingDB/Reading.java
@@ -1,6 +1,12 @@
 package InterfacingDB;
 import java.io.*;
 
+/*
+ *    E' una versione "primordiale" per leggere l'inventario da un semplice file CSV.
+ *    Più tardi verrà fatta una versione più bella per leggere l'inventario da una tabella
+ *    hostata su un server SQL.
+ */
+
 public class Reading{
   private BufferedReader buffer;
   
