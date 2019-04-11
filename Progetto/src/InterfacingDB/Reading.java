8431db282f3a3178105e6c16e7d16398928f8e79
cl448776 <fabio.riganti01@universitadipavia.it>
2019-04-11 15:44:58 +0200

    Piccolo abbellimento al metodo read

--- Progetto/src/InterfacingDB/Reading.java
+++ Progetto/src/InterfacingDB/Reading.java
@@ -9,21 +9,20 @@ import java.io.*;
 
 public class Reading{
   private BufferedReader buffer;
-  
+
   public Reading() throws IOException {
     buffer = new BufferedReader(new FileReader("prova.csv"));
   }
-  
+
   public Reading(String str) throws IOException {
     buffer = new BufferedReader(new FileReader(str));
   }
 
   public String[] read() throws IOException {
     String line = buffer.readLine();
-    if(line!=null){
+    if(line!=null)
       return line.split(";");
-    } else {
-      return null;
-    }
+    return null;
   }
 }
+
