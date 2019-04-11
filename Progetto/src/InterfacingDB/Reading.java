ef0185273b08fc8adf36d8054e55f69df0faac2d
fabioriganti <48350774+fabioriganti@users.noreply.github.com>
2019-04-11 15:12:58 +0200

    Aggiunta funzione read per leggere stringa da file

--- Progetto/src/InterfacingDB/Reading.java
+++ Progetto/src/InterfacingDB/Reading.java
@@ -1,4 +1,18 @@
 package InterfacingDB;
+import java.io.*
 
 public class Reading {
+  private BufferedReader buffer;
+  public Reading(){
+    buffer = new BufferedReader(new FileReader("prova.csv"));
+  }
+  
+  public String[] read(){
+    String line = buffer.ReadLine();
+    if(line!=null){
+      return line.split(";");
+    } else {
+      return null;
+    }
+  }
 }
