21c26dbdde17c26a342f932c003944004a850efa
Manuel Zippo <manuel.zippo01@universitadipavia.it>
2019-04-16 15:30:48 +0200

    Add Resources for class Case

--- Progetto/src/Components/Case.java
+++ Progetto/src/Components/Case.java
@@ -1,6 +1,25 @@
 package Components;
 
+import Resources.Resource;
+
 public class Case extends AbstractComponent {
+
+    public Case(String[] input) {
+        super(input);
+
+        resource = new Resource.Builder()
+                .withokCase(true)
+                .withDimensionCase(caratteristiche[1])
+                .withNSlot(Integer.parseInt(caratteristiche[3]))
+                .build();
+    }
+
+
+
+
+
+
+
     /*@Override
     public int compareTo(Object o) {
         return 0;
