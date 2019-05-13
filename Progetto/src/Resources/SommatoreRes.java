738c962bf32a8383182128d7c2efc09a072782af
FrancescoRocchetti <fra.rokketti@gmail.com>
2019-05-13 09:33:38 +0200

    creato sommatore di risorse

---
+++ Progetto/src/Resources/SommatoreRes.java
@@ -0,0 +1,31 @@
+package Resources;
+
+import java.util.ArrayList;
+
+public class SommatoreRes {
+
+    private Resource loop(ArrayList<Resource> ar){
+        Resource temp = new Resource.Builder().build();
+
+        for(Resource r : ar){
+            temp = new Resource.Builder()
+                    .withokPSU(temp.isOkPSU() || r.isOkPSU())
+                    .withokMOBO(temp.isOkMOBO() || r.isOkMOBO())
+                    .withokCPU(temp.isOkCPU() || r.isOkMOBO())
+                    .withokCase(temp.isOkCase() || r.isOkCase())
+
+                    .withNSATA(temp.getnSATA() + r.getnSATA())
+                    .withNSlot(temp.getnSlot325() + r.getnSlot325())
+                    .withNPci(temp.getnPci() + r.getnPci())
+                    .withNPcie(temp.getnPcie() + r.getnPcie())
+                    .withModules(temp.getModulesRAM() + r.getModulesRAM())
+                    .build();
+        }
+
+        return temp;
+    }
+
+    public Resource sum(ArrayList<Resource> ar){
+        return loop(ar);
+    }
+}
