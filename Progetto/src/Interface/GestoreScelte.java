78716fe1bb9b284093d9cc2be3eca48359975f2c
Matteo Lucchini <matteo.lucchini97@gmail.com>
2019-05-24 15:58:46 +0200

    Modifica alle modalità di rimozione item

--- Progetto/src/Interface/GestoreScelte.java
+++ Progetto/src/Interface/GestoreScelte.java
@@ -15,21 +15,22 @@ public class GestoreScelte extends Piattaforma {
     private int row = 100;
     private int nr = 0;
     private SelectedComponents scp;
+    private PCParts[] cmp = new PCParts[]{PCParts.MOBO, PCParts.CPU, PCParts.RAM, PCParts.STORAGE, PCParts.GPU, PCParts.PSU, PCParts.COOLER, PCParts.OS, PCParts.CASE};
+    private JPanel[] pnl = new JPanel[]{panels[0], panels[1], panels[2], panels[3], panels[4], panels[5], panels[6], panels[7], panels[8]};
 
     public GestoreScelte() {
         super();
         scp = new SelectedComponents();
         str = new ArrayList<>();
-        PCParts[] cmp = new PCParts[]{PCParts.MOBO, PCParts.CPU, PCParts.RAM, PCParts.STORAGE, PCParts.GPU, PCParts.PSU, PCParts.COOLER, PCParts.OS, PCParts.CASE};
-        JPanel[] pnl = new JPanel[]{panels[0], panels[1], panels[2], panels[3], panels[4], panels[5], panels[6], panels[7], panels[8]};
         newConfigListener();
         loginListener();
+        rechargeListener();
         exitListener();
         try {
             obtainParts(cmp, pnl);
         } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Errore: impossibile connettersi al DB", "Errore", JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
-            //System.err.println("Errore nella lettura");
         }
     }
 
@@ -89,6 +90,20 @@ public class GestoreScelte extends Piattaforma {
             price.setText("0 €");
             items.setText("");
             str.clear();
+        });
+    }
+
+    private void rechargeListener() {
+        recharge.addActionListener(e -> {
+            try {
+                for(JPanel p : pnl)
+                    p.removeAll();
+                obtainParts(cmp, pnl);
+            } catch (SQLException e1) {
+                JOptionPane.showMessageDialog(null, "Errore: impossibile connettersi al DB.\nIl programma terminerà la sua esecuzione.", "Errore", JOptionPane.ERROR_MESSAGE);
+                e1.printStackTrace();
+                System.exit(10);
+            }
         });
     }
 
