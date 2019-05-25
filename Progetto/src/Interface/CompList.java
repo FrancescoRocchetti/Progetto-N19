220f4beb5307b4f4713d051b23267067cf487312
Matteo Lucchini <matteo.lucchini97@gmail.com>
2019-05-25 18:52:01 +0200

    Modifiche alla tabella

--- Progetto/src/Interface/CompList.java
+++ Progetto/src/Interface/CompList.java
@@ -16,13 +16,17 @@ import java.util.ArrayList;
 public class CompList extends JFrame {
 
     private ArrayList<AbstractComponent> comp;
+    private Toolkit kit;
+    private Dimension dim;
 
     public CompList(InserimentoSpecifiche ins){
         super("Inventario");
+        kit = Toolkit.getDefaultToolkit();
+        dim = kit.getScreenSize();
         try {
             ins.setEnabled(false);
             ins.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-            setLocationRelativeTo(ins);
+            //setLocationRelativeTo(ins);
             Container c = getContentPane();
             Reading r = new Reading();
             comp = r.read(null);
@@ -30,6 +34,12 @@ public class CompList extends JFrame {
             String column[] = {"ID", "TIPO", "NOME", "QUANTITÁ", "PREZZO"};
             JTable table = new JTable(data, column);
             table.setDefaultEditor(Object.class, null);
+            table.getColumnModel().getColumn(0).setPreferredWidth(40);
+            table.getColumnModel().getColumn(1).setPreferredWidth(55);
+            table.getColumnModel().getColumn(2).setPreferredWidth(250);
+            table.getColumnModel().getColumn(3).setPreferredWidth(30);
+            table.getColumnModel().getColumn(4).setPreferredWidth(50);
+            table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
             JScrollPane sp = new JScrollPane(table);
             JPanel panel = new JPanel();
             JButton btn = new JButton("Ok");
@@ -40,29 +50,23 @@ public class CompList extends JFrame {
 
             setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             setResizable(false);
-            setSize(300,400);
-            setResizable(false);
+            //setSize(300,400);
             setVisible(true);
+            pack();
+            setLocation(dim.width / 2 - this.getWidth() / 2, dim.height / 2 - this.getHeight() / 2);
 
-            btn.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
+            btn.addActionListener(e -> {
                     ins.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
                     ins.setEnabled(true);
                     CompList.super.dispose();
-                }
             });
 
             addWindowListener(new WindowListener() {
                 @Override
-                public void windowOpened(WindowEvent e) {
-
-                }
+                public void windowOpened(WindowEvent e) { }
 
                 @Override
-                public void windowClosing(WindowEvent e) {
-
-                }
+                public void windowClosing(WindowEvent e) { }
 
                 @Override
                 public void windowClosed(WindowEvent e) {
@@ -71,24 +75,16 @@ public class CompList extends JFrame {
                 }
 
                 @Override
-                public void windowIconified(WindowEvent e) {
-
-                }
+                public void windowIconified(WindowEvent e) { }
 
                 @Override
-                public void windowDeiconified(WindowEvent e) {
-
-                }
+                public void windowDeiconified(WindowEvent e) { }
 
                 @Override
-                public void windowActivated(WindowEvent e) {
-
-                }
+                public void windowActivated(WindowEvent e) { }
 
                 @Override
-                public void windowDeactivated(WindowEvent e) {
-
-                }
+                public void windowDeactivated(WindowEvent e) { }
             });
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null,"Errore lettura inventario", "Errore", JOptionPane.ERROR_MESSAGE);
@@ -110,11 +106,11 @@ public class CompList extends JFrame {
         return data;
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         try {
             new CompList(new InserimentoSpecifiche(null, "FABBBBIO"));
         } catch (Exception e) {
             System.err.println("asd");
         }
-    }
+    }*/
 }
