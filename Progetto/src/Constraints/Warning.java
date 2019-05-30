d7aef1ebe13cf3514eb143055c25eb308f2301a8
FrancescoRocchetti <fra.rokketti@gmail.com>
2019-05-30 09:16:51 +0200

    iniziato la classe per le build automatiche

--- Progetto/src/Constraints/Warning.java
+++ Progetto/src/Constraints/Warning.java
@@ -6,6 +6,7 @@ import Resources.Resource;
 public class Warning {
     private Warning wInstance = null;
     private String info= null;
+    private boolean ok;
 
     private Warning(){
     }
@@ -19,22 +20,27 @@ public class Warning {
 
     public void check(SelectedComponents sc){
         info = "";
+        ok = true;
         Resource r = sc.getTotRes();
 
         if((r.isOkMOBO() && r.getnSATA() < 0) || (r.isOkCase() && r.getnSlot325() < 0)){
             info.concat("too many HDDs ");
+            ok = false;
         }
 
         if(r.isOkMOBO() && r.getModulesRAM() < 0){
             info.concat("too many RAM modules ");
+            ok = false;
         }
 
         if(r.isOkMOBO() && r.getnPci() < 0){
             info.concat("too many GPUs ");
+            ok = false;
         }
 
         if(r.isOkPSU() && r.getPower() < 0){
             info.concat("not enough power ");
+            ok = false;
         }
 
 
