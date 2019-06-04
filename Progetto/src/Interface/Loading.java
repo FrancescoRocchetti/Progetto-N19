3f5f12cb635eb4260fa1e63f2a0f664fd60470c3
Matteo Lucchini <matteo.lucchini97@gmail.com>
2019-06-04 14:20:44 +0200

    Aggiunto caricamento spastico

--- Progetto/src/Interface/Loading.java
+++ Progetto/src/Interface/Loading.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.net.URL;
+import java.util.Random;
 
 public class Loading extends JFrame {
     public Loading() {
@@ -12,20 +13,22 @@ public class Loading extends JFrame {
         URL url = getClass().getResource("Resources/loading.gif");
         ImageIcon img = new ImageIcon(url);
         JLabel label = new JLabel(img);
-        JLabel txt = new JLabel("Sto scaricando i dati...", SwingConstants.CENTER);
+        JLabel txt = new JLabel();
+        String[] loadingStrings = {"Sto contando le stelle...", "Sto allineando i pianeti...", "Ti sto guardando...", "Sto svuotando le nuvole..."};
+        Random random = new Random();
+        int ran = random.nextInt(loadingStrings.length);
+        System.out.println(ran);
+        txt.setText(loadingStrings[ran]);
+        txt.setHorizontalAlignment(SwingConstants.CENTER);
         txt.setBorder(new EmptyBorder(0, 0, 30, 0));
+
         c.setLayout(new BorderLayout());
         c.add(label, BorderLayout.CENTER);
         c.add(txt, BorderLayout.SOUTH);
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(200, 200);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }
-
-    /*public static void main(String[] args) {
-        new Loading();
-    }*/
 }
