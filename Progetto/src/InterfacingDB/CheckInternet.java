26c7c6d1319dad470065fb831b51fa0a4c081d0f
fabio <fabio.riganti01@universitadipavia.it>
2019-06-12 17:54:39 +0200

    Corretto funzionamento di CheckInternet (strano che il ping non funzioni in tutti i PC...)

--- Progetto/src/InterfacingDB/CheckInternet.java
+++ Progetto/src/InterfacingDB/CheckInternet.java
@@ -1,19 +1,24 @@
 package InterfacingDB;
 
-import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class CheckInternet {
 
     public static boolean check() {
         try {
-            String ipAddress = "37.59.55.185";
+            /*String ipAddress = "37.59.55.185";
             InetAddress inet = InetAddress.getByName(ipAddress);
             //System.out.println("Sending Ping Request to " + ipAddress);
             if (inet.isReachable(5000)) {
                 System.out.println("Ok");
                 return true;
             }
-            else return false;
+            else return false;*/
+            URL url = new URL("https://remotemysql.com/");
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            return true;
         } catch (Exception e) {
             return false;
         }
