26eda41d18d41baae3354e5b31b35bf41c8a82b0
Matteo Lucchini <matteo.lucchini97@gmail.com>
2019-06-17 16:12:51 +0200

    Idea di base per auto configurazione

--- Progetto/src/Interface/NoBudgetConfig.java
+++ Progetto/src/Interface/NoBudgetConfig.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class NoBudgetConfig {
     public NoBudgetConfig(){
-
+        /*
+        * Logica di funzionamento (base):
+        * Per ogni componente, scelgo quello con ranking max && prezzo max
+        * */
     }
 }
