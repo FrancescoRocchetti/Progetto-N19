6b8aefcbba8997df9be68ace029dd5b4c71e600f
FrancescoRocchetti <fra.rokketti@gmail.com>
2019-06-19 21:53:33 +0200

    primo tentativo di build automatica

---
+++ Progetto/src/Automatic/Test.java
@@ -0,0 +1,8 @@
+package Automatic;
+
+public class Test {
+    public static void main(String[] args) {
+        ABuild ab = new ABuild(1000);
+        System.out.println(ab.getSc());
+    }
+}
--- Progetto/src/Interface/NoBudgetConfig.java
+++ Progetto/src/Interface/NoBudgetConfig.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class NoBudgetConfig {
     public NoBudgetConfig(){
         /*
+        VEDI PACKAGE Automatic
+
         * Logica di funzionamento (base):
         * Per ogni componente, scelgo quello con ranking max && prezzo max
         * */
