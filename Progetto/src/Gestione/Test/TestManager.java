90ad27eb77304e65d57a778af9b3908138c5f61b
fabio <fabio.riganti01@universitadipavia.it>
2019-06-26 14:27:58 +0200

    JUnit per Manager

---
+++ Progetto/src/Gestione/Test/TestManager.java
@@ -0,0 +1,27 @@
+package Gestione.Test;
+
+import Gestione.Manager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestManager {
+
+    @Test
+    public void testCompToObj(){
+        Manager m = new Manager();
+
+        assertThrows(NullPointerException.class, () -> {
+            m.getObjectFromComps(null);
+        });
+    }
+
+    @Test
+    public void testCreateLogin(){
+        Manager m = new Manager();
+
+        assertThrows(NullPointerException.class, () -> {
+            m.createLogin(null);
+        });
+    }
+}
--- Progetto/src/InterfacingDB/Test/ReaderTest.java
+++ Progetto/src/InterfacingDB/Test/ReaderTest.java
@@ -1,10 +1,8 @@
 package InterfacingDB.Test;
 
-import Components.PCParts;
 import InterfacingDB.Reader;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReaderTest {
     @Test
